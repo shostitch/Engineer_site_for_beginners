352b61eeae343f73cccddc177707e02ad57d5c27
shostitch <102853678+shostitch@users.noreply.github.com>
2022-08-17 12:04:43 +0000

    細かい調整

--- app/models/post.rb
+++ app/models/post.rb
@@ -41,11 +41,11 @@ class Post < ApplicationRecord
     old_tags = current_tags - sent_tags
     new_tags = sent_tags - current_tags
 
-    old_tags.each do |old|
+    old_tags.uniq.each do |old|
       self.tags.delete Tag.find_by(tag_name: old)
     end
 
-    new_tags.each do |new|
+    new_tags.uniq.each do |new|
       new_post_tag = Tag.find_or_create_by(tag_name: new)
       self.tags << new_post_tag
     end
